32a9d03f9277ba1bfea328252c75b3e090b3ee93
a-rudenko <i.lex.rudenko@gmail.com>
2025-01-25 16:52:05 +0300

    fix: icon handling for demo.

--- src/demo/demo.js
+++ src/demo/demo.js
@@ -6,10 +6,9 @@ import { loadTextures } from '../texture.js';
 import { loadSettings, exportSettings } from '../settings.js';
 import '../assets/css/main.css';
 
-export async function initWebGLTechParticles(containerId) {
+export async function initWebGLTechParticles(containerId, iconFolderPath) {
     const settings = await loadSettings();
-    const textures = loadTextures();
-
+    const textures = await loadTextures(iconFolderPath);
     const scene = new Scene(settings, containerId);
     const particles = new Particles(scene, settings, textures);
     const renderer = new Renderer(scene, particles, containerId);
